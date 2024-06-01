64a37547e96cafac7cb889c98919a5d23c2ca965
Pavel <offroad201d7@gmail.com>
2024-06-01 04:35:00 +0000

    add homework1

---
+++ homework1/Program.cs
@@ -0,0 +1,22 @@
+﻿static string PrintNumbers(int M, int N)
+{
+    if (M == N)
+    {
+        return M.ToString();
+    }
+    else if (M < N)
+    {
+        return M + ", " + PrintNumbers(M + 1, N);
+    }
+    else
+    {
+        return M + ", " + PrintNumbers(M - 1, N);
+    }
+}
+
+
+int M = 4;
+int N = 8;
+
+string result = PrintNumbers(M, N);
+Console.WriteLine(result);
