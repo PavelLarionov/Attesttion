7cddc0b84ac145f1f1ddac60a7c094ca7ef15616
Pavel <offroad201d7@gmail.com>
2024-06-02 00:12:12 +0000

    add homework3

---
+++ homework3/Program.cs
@@ -0,0 +1,17 @@
+﻿// Задайте произвольный массив. Выведете его элементы, 
+// начиная с конца. Использовать рекурсию, не использовать циклы.
+
+void PrintArrayReverse(int[] array, int index)
+{
+    if (index < 0)
+    {
+        return;
+    }
+
+    Console.Write($"{array[index]} ");
+    PrintArrayReverse(array, index - 1);
+}
+
+    int[] array = { 1, 2, 3, 4, 5 };
+    PrintArrayReverse(array, array.Length - 1);
+
